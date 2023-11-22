77f0a8b5d741c58a68e237091d02e9639edab903
Álvaro González <Alvaro.Gonzalez@csc.fi>
2023-11-22 15:34:58 +0200

    Change form red to white, might change it to light green

--- lua/user/whitespace.lua
+++ lua/user/whitespace.lua
@@ -1,6 +1,8 @@
 -- highlight ExtraWhitespace ctermbg='red'
 
 vim.g.better_whitespace_enabled=1
-vim.g.better_whitespace_ctremcolor='darkred'
+vim.g.better_whitespace_ctremcolor='white'
+vim.g.better_whitespace_guicolor='white'
+
 
 vim.g.better_whitespace_filetypes_blacklist={'diff'}
